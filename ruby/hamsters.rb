08671c8844db1177efc469c8c9078907cad95fa1
Hugo <hugochen1212@gmail.com>
2017-05-09 21:15:36 -0700

    Add a if and else statement in the part of hamster's age

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,16 +1,30 @@
 puts "What is the hamster's name?"
  name=gets.chomp
-puts "What volume level from 1 to 10 is the hamster?"
+
+puts "What volume level of the hamster? (Range 1 to 10)"
  volume = gets.chomp.to_i
+
 puts "what is the fur color?"
  fur_color=gets.chomp
-puts "Is the hamster adoptable? (Y/N)"
+
+puts "Is the hamster adoptable? (Yes/No)"
  adoptable = gets.chomp
-puts "How old is the hamster?"
+
+puts "How many years old is the hamster? (estimate)"
+age=gets.chomp
+if age == ""
  age=nil
- age=gets.chomp
+else
+ age = age.to_i
+end
+
 puts "The name is #{name}."
 puts "The volume is #{volume}."
 puts "The fur color is #{fur_color}."
 puts "the adoptability is #{adoptable}."
-puts "the age is #{age}."
+
+if age == nil
+  puts "No age information available"
+else
+ puts "The hamster is #{age} years old"
+end
