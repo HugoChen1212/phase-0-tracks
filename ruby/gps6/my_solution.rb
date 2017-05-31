8045265a89d5b9386dcf39c3e6b968096a4d663f
Hugo <hugochen1212@gmail.com>
2017-05-31 00:41:07 -0700

    Add refactor and some commented of reflections

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,19 +1,21 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with:Katie McNeil].
+# We spent [2.5] hours on this challenge.
 
 # EXPLANATION OF require_relative
-# Require_relative: allowing you to load a file that is relative to the file contining or can handle the parths relative to current directory
-# Require: is taking using other data
+# Require_relative: allowing you to load a file that is relative to the file contining or can handle the parths relative to current directory. It usually use to ask to retrieve a file from a specific location. We can use require_relative by calling "require_relative 'the_path_of_file_name"
+
+# Require: gives you access to the many extensions and programming libraries bundled with the Ruby programming language-as an even larger number of extensions. We can use require by calling "require 'name_of_the_file"
+
 require_relative 'state_data'
 
 class VirusPredictor
   # initialize the instance variable and attribute value
-  def initialize(state_of_origin, population_density, population)
+  def initialize(state_of_origin)
     @state = state_of_origin
-    @population = population
-    @population_density = population_density
+    @population = STATE_DATA[state_of_origin][:population]
+    @population_density = STATE_DATA[state_of_origin][:population_density]
   end
 
 # call the other two methods
@@ -26,18 +28,20 @@ class VirusPredictor
   # create method that calculate and do the predicted of deaths and basedupon the population_density and population
   def predicted_deaths
     # predicted deaths is solely based on population density
+
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+        rate = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+        rate = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+        rate = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+        rate = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+        rate = 0.05
     end
 
+  number_of_deaths = (@population * rate).floor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
@@ -47,18 +51,17 @@ class VirusPredictor
     #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -87,9 +90,15 @@ alaska.virus_effects
 =end
 
 STATE_DATA.each_key do |state|
-  state = VirusPredictor.new(state, STATE_DATA[state][:population_density], STATE_DATA[state][:population])
-  state.virus_effects
+   VirusPredictor.new(state).virus_effects
 end
 
 #=======================================================================
 # Reflection Section
+# Release 8:
+# It use two different syntax to write this two hash, one is the old way (""=>""), but user friendly, which is easy to read. The other one is the symbol, which is more efficiency for programmer use.
+# Require_relative: allowing you to load a file that is relative to the file contining or can handle the parths relative to current directory. It usually use to ask to retrieve a file from a specific location.
+# Require: gives you access to the many extensions and programming libraries bundled with the Ruby programming language.
+# Some ways to iterate through a hash are use the method like: each_key, each, and each_value.
+# I never thought it can delete the the parameters and argument, and make the code very easy to read. I think refactoring is a very good skill, and I will try use it in the future.
+# Except the concept of refactoring others are feeling solidify.
