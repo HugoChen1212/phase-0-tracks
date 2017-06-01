a471bd6070d636a74767942f6451eef02a831dc5
Hugo <hugochen1212@gmail.com>
2017-05-31 19:45:00 -0700

    update driver code

--- js/explore.js
+++ js/explore.js
@@ -7,13 +7,26 @@
 // output: string
 
 function reverse (str) {
-  var reverse_string =" "
+  var reverse_string =" ";
 for (var i = str.length-1; i >= 0; i-=1) {
-   reverse_string += str[i]
+   reverse_string += str[i];
 }
-   return reverse_string
+   return reverse_string;
 }
 
-console.log(reverse("hello"))
+console.log(reverse("hello"));
 
+word = "hello";
+var ispalindrome = false;
+var words = true;
+if (ispalindrome){
+  console.log("Would not to reverse the string.");
+}
+else
+{
+  console.log("Would be reverse the string: " + reverse(word));
+}
 
+if (1==1){
+  console.log("Yes the conditino is ture and the variable is:" +word);
+}
