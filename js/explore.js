0de9f32ba52513bcde469c6d52911235899911ac
Hugo <hugochen1212@gmail.com>
2017-05-31 19:08:06 -0700

    Add pseudocode

--- js/explore.js
+++ js/explore.js
@@ -6,13 +6,6 @@
     // for each letter reverse order add in to the new empty string
 // output: string
 
-function reverse (str) {
-  var reverse_string =" "
-for (var i = str.length-1; i >= 0; i-=1) {
-   reverse_string += str[i]
-}
-   return reverse_string
-}
 
-console.log(reverse("hello"))
+
 
