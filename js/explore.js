152095b38204ecf8f7dd8102f25306eae8ee6b5b
Hugo <hugochen1212@gmail.com>
2017-05-31 19:08:54 -0700

    Add test code

--- js/explore.js
+++ js/explore.js
@@ -6,6 +6,14 @@
     // for each letter reverse order add in to the new empty string
 // output: string
 
+function reverse (str) {
+  var reverse_string =" "
+for (var i = str.length-1; i >= 0; i-=1) {
+   reverse_string += str[i]
+}
+   return reverse_string
+}
+
 
 
 
