85a191534f43e24007d4f8635fef00b75d6f34c8
Hugo <hugochen1212@gmail.com>
2017-06-04 15:06:20 -0700

    Add generate random function and the driver code

--- js/algos.js
+++ js/algos.js
@@ -54,3 +54,40 @@ console.log(Key_Value_Match({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}))
 console.log(Key_Value_Match({animal: "Dogs", legs: 4}, {animal: "Dog", legs: 3}));
 
 
+// Function to Generate Random Test Data
+// input: interger and array
+// steps:
+  // create a new_array
+  // set the range of the words we need
+  // add the words in the a new_array
+  // create another array
+  // set the number words need to be random
+  // get starte random words from the new_array
+  // add the output of the random words to another array
+// return another array
+// output: another array
+
+function Random_data (num, array) {
+ var range_words = [];
+for (var i = 0; i < array.length; i++){
+  if (array[i].length >= 0 && array[i].length < 10){
+    range_words.push(array[i]);
+  }
+}
+ var output = [];
+for (var k = 0; k < num; k++){
+  var random_index  = Math.floor(Math.random() * range_words.length);
+  output.push(range_words[random_index]);
+}
+return output;
+}
+
+example=["long ph","longest","car","apple","phrase","longer", "a", "dfkjdlfjaldfjldfj"];
+
+
+for (var n = 0; n < 10; n++){
+ramdom_array=Random_data(3,example);
+console.log(ramdom_array);
+console.log(Logest_Phrase(ramdom_array));
+}
+
