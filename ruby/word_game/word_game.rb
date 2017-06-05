7b1c640f495aa51e2c1d062acee6a8edc4e9f7c2
Hugo <hugochen1212@gmail.com>
2017-06-04 22:58:49 -0700

    Update word game with driver code

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -6,55 +6,44 @@
   #output: initialize atrributes
 
 # method to create play_game
-  # input: word, letter
+  # input: letter
   # steps:
-    # Ask user to get word
-      # get word from user
-    # create and set the default into the list
-    # set the limited number of guessing
-    # Ask player to guess letter
-      # get player's letter
     # IF letter include in the word
     # convert the string to list
     # then iterate through the word
       # IF split_word's letter == player's letter
         # set into the list with the correct position and letter
-    # print out the list and convert to string
+    # print out the list with the guess right letter and convert to string
     # IF not include the letter
-      # it will print a message "Please try again!"
+      # it will return a default setting of the array
     # return list and convert to string
   # output: string
 
 
 class Word_game
-    attr_reader :guess_count
-  def initialize
+    attr_reader :guess_count, :word, :correct_letter
+  def initialize (word)
+      @word =word
       @guess_count = 0
-      @correct_letter = []
+      @correct_letter = Array.new(word.length,"_")
   end
 
-  def play_game (word)
-    @correct_letter = Array.new(word.length,"_")
-        idx=0
-    while idx < word.length
-       puts "Other user please guess your letter."
-        letter = gets.chomp
-      if word.include? (letter)
+  def play_game (letter)
+      if @word.include? (letter)
          @guess_count+=1
-          arr_word = word.split("")
-          idx2 =0
-          while idx2 < arr_word.length
-             if arr_word[idx2] == letter
-                @correct_letter[idx2] = letter
+          arr_word = @word.split("")
+          idx =0
+          while idx < arr_word.length
+             if arr_word[idx] == letter
+                @correct_letter[idx] = letter
               end
-              idx2+=1
+              idx+=1
          end
             p @correct_letter.join("")
       else
           puts "Please try again!"
       end
-      idx+=1
-   end
+
     return @correct_letter.join ("")
   end
 end
@@ -62,9 +51,16 @@ end
 
 puts "User pleae enter your word here."
   word = gets.chomp
-game = Word_game.new
- game.play_game(word)
+game = Word_game.new(word)
+
+word.length.times do
+    puts "Other user please guess your letter."
+        letter = gets.chomp
+        game.play_game(letter)
+end
+
 puts "You won in #{game.guess_count} guesses!"
 if game.guess_count==word.length
+  puts "User enter word is #{game.word}."
   puts "You win the game!"
 end
