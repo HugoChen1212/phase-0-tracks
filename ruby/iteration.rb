47668193817097f5dcd05d5ba95193908d6036da
Hugo <hugochen1212@gmail.com>
2017-06-10 21:22:21 -0700

    update iteration file

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,55 +1,47 @@
-#Release 1: Question number1
+# Release 1
+#Question number1
 vehicel=["car", "bus", "motorcycle", "truck"]
-
-actors={"Leonardo"=>5,"Tom"=>3,"Brad"=>8, "Ryan"=>10}
+hotel={"Motel 6"=>3,"Grant Hotel"=>4,"Super 8"=>3.5, "Hilton"=>5}
 
 #Question number2
-vehicel.each {
-  |name| puts "Here is each vehicels: #{name}"
+vehicel.each {|name|
+  puts "Here is each vehicels: #{name}"
 }
 
-ex_of_map = vehicel.map! {|name|
- puts name
- name.capitalize
-}
-p ex_of_map
+hotel.each do |name,level|
+ puts "#{name}'s level is #{level}."
+end
 
-actors.each{
-  |name, level|  puts "Here is the name of the actors #{name}, and here is the leve of this actors #{level}"
-}
+new_vehicel = vehicel.map! do |name|
+  name.next
+end
+p new_vehicel
 
-new_has=actors.map {  |name, level|
-  actors["Ryan"]= 9
-  puts "Here is the name of the actors #{name}, and here is the leve of this actors #{level}"
-}
-p new_has
+# Release 2
+#Question number1
+ four_start_up_hotel = hotel.reject do |name,level|
+  level < 4
+end
+p four_start_up_hotel
 
-# Release 2's Question number 1
-low_level_actors = actors.delete_if {
-  |name,level| level  < 5
-}
-p low_level_actors
-p actors
+# number2
+four_start_down_hotel = hotel.select do |name,level|
+  level < 4
+end
+p four_start_down_hotel
 
-# Question number 3
-middle_actors = actors.select {
-  |name,level| level == 5
-}
-p middle_actors
-p actors
+# number3
+four_start_down_hotel = hotel.keep_if do |name,level|
+ level < 4
+end
+p four_start_down_hotel
 
-# Question number 2
-high_level_actors=actors.keep_if {
-  |name,level| level > 6
-}
-p high_level_actors
-p actors
-
-# Question number 4
+# number4
  number_arr=[0,1,2,3,4,5,6,7,8,9]
 drop_num = number_arr.drop_while {|number|
   number < 5
 }
+puts "drop number less than 5."
 p drop_num
-p number_arr
+
 
