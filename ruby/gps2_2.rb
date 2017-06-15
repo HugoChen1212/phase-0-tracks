a3ab4f60db7e3e4cf12b6fd4c741cc1a41b2821f
Hugo <hugochen1212@gmail.com>
2017-06-14 19:17:08 -0700

    add pseudocode

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,40 @@
+#RELEASE 0 - PSEUDOCODE
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # break the array of a item name
+  # create hash data structure for list
+  # assign a food name to the list as key
+  # set default quantity as a value
+  # print the list
+# output: hash
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps:
+# assign a food name to the list as key
+# assign quantity to the list as value
+# return list
+# output:hash
+
+# Method to remove an item from the list
+# input: list, item name
+# steps:
+  # delete the item
+# output: hash
+
+# Method to update the quantity of an item
+# input: list, item name, quantity
+# steps:
+  # IF item name in the list
+  # update the quantity in the list
+  # IF item name not in the list
+  # send out a message to the user
+# output: hash
+
+# Method to print a list and make it look pretty
+# input: list
+# steps:
+  # print and iterate to each item and quantity
+# output: print a list
+
+
