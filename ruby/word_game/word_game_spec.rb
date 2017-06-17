56bf7c80e683264c4462a049bde68fcdb11fe705
Hugo <hugochen1212@gmail.com>
2017-06-17 16:39:24 -0700

    add check_letter test

--- ruby/word_game/word_game_spec.rb
+++ ruby/word_game/word_game_spec.rb
@@ -1,40 +1,46 @@
 require_relative 'word_game'
 
-describe Word_game do
-  let(:word_game) {Word_game.new("hugo")}
+describe Guess_word do
+  let(:guess_word) {Guess_word.new("hugo")}
 
   describe "initialize" do
  it "initialize the word" do
-    expect(word_game.word). to eq ("hugo")
+    expect(guess_word.word). to eq ("hugo")
  end
  it "initialize the guess_count" do
-    expect(word_game.guess_count). to eq (0)
+    expect(guess_word.guess_count). to eq (0)
  end
 
  it "initialize the array" do
-    expect(word_game.correct_letter). to eq (["_","_","_","_"])
+    expect(guess_word.correct_letter). to eq (["_","_","_","_"])
  end
-end
-describe "play_game" do
- it "return the letter if guess right" do
-    expect(word_game.play_game("h")).to eq("h___")
+ end
+  describe "play_game" do
+    it "return the letter if guess right" do
+      expect(guess_word.play_game("h")).to eq("h___")
+    end
+
+    it "return the letter if guess right" do
+     expect(guess_word.play_game("u")).to eq("_u__")
+    end
+
+    it "return the letter if guess right" do
+      expect(guess_word.play_game("g")).to eq("__g_")
+    end
+
+    it "return the letter if guess right" do
+      expect(guess_word.play_game("o")).to eq("___o")
+    end
+
+    it "if the letter guess wrong" do
+      expect(guess_word.play_game("i")).to eq("____")
+    end
   end
 
-  it "return the letter if guess right" do
-    expect(word_game.play_game("u")).to eq("_u__")
+  describe "check_letter" do
+    it "return an array with the correct guess letter" do
+      letter=["h", "u","g","o"]
+      expect(guess_word.check_letter(letter,"h")).to eq(["h","_","_","_"])
+    end
   end
-
-  it "return the letter if guess right" do
-    expect(word_game.play_game("g")).to eq("__g_")
-  end
-
-  it "return the letter if guess right" do
-    expect(word_game.play_game("o")).to eq("___o")
-  end
-
-   it "if the letter guess wrong" do
-    expect(word_game.play_game("i")).to eq("____")
-  end
-
-end
 end
