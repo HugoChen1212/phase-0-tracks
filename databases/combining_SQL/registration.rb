ef4fffb2c80b5bbfc0c8833df39fb77d4416ae82
Hugo <hugochen1212@gmail.com>
2017-07-03 22:15:26 -0700

    add user data into database

--- databases/combining_SQL/registration.rb
+++ databases/combining_SQL/registration.rb
@@ -12,3 +12,24 @@ CREATE TABLE IF NOT EXISTS user(
 SQL
 
 vote_db.execute(create_table_cmd)
+
+def create_users(vote_db, name, driver_licence, under_eighteen)
+  vote_db.execute("INSERT INTO user (name, driver_licence, under_eighteen) VALUES (?,?,?)", [name, driver_licence, under_eighteen])
+end
+
+puts "Hi everyone, welcome to vote your favor president!"
+puts "Please register here and when your done type \"exit\"."
+puts "Please enter your full name here."
+name = gets.chomp
+puts "Please enter your Driver licence number here."
+driver_licence = gets.chomp.to_i
+puts "Do you under eighteen? please type (yes or no)"
+under_eighteen = gets.chomp
+
+create_users(vote_db, name, driver_licence, under_eighteen)
+
+# if under_eighteen == "yes"
+#   puts "congratulation, you can vote now!"
+# else
+#   puts "Sorry you are under eighteen, and you are not able to register until you become eighteen."
+# end
