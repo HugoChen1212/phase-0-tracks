14b389d61ce229d8843be5b9246996a82d7962ca
Hugo <hugochen1212@gmail.com>
2017-07-13 19:12:45 -0700

    Add code for the whole thing

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,11 +17,50 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/students/about' do
+  @students = db.execute("SELECT * FROM students")
+  erb :about
+end
+
 # create new students via
 # a form
-post '/students' do
+get '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
 
+get '/students/form' do
+  erb :campuses
+end
+
+
+
+
+
+
+
+post '/about/table' do
+  p params
+  db.execute("insert into campuses (campus) values (?)",[params['campus']])
+   redirect '/students/campus'
+end
+
+
+get '/students/campus' do
+  @campuses = db.execute("SELECT * FROM campuses")
+  erb :new_campuses
+end
+
+
+
+=begin
+
+ "IF NOT EXISTS (INSERT INTO campuses (campus) values (?))CREATE TABLE campuses (id integer primary key, campus varchar(255))",[params['campus']])
+
+ create_table_compus = <<-SQL
+
+SQL
+
+
+=end
 # add static resources
