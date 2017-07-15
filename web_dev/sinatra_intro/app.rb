8fd8946c4a4063966a504cb1e2af13b625d3361b
Hugo <hugochen1212@gmail.com>
2017-07-15 14:10:08 -0700

    9.4 Building a Web Application in Ruby

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -41,6 +41,36 @@ end
 # a particular student
 
 get '/students/:id' do
-  student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+  student = db.execute("SELECT campus FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
+end
+
+get '/:house_num/:street/:city/:state' do
+ house_num = params[:house_num]
+ street = params[:street]
+ city = params[:city]
+ state = params[:state]
+  "Your address is: #{house_num} #{street} #{city}, #{state}"
+end
+
+get '/contact' do
+   "Your address is:<br> 376 Arch street <br> San Francisco, CA 94622"
+end
+
+get '/great_job/:name' do
+  names = db.execute("SELECT name FROM students")
+  arr=[]
+  names.each do |name|
+   arr << name[0]
+  end
+if arr.include? (params[:name])
+  "Good job, #{params[:name]}"
+  else
+    "Good job!"
+  end
+end
+
+get '/twonum/:num1/:num2' do
+  total = params[:num1].to_i + params[:num2].to_i
+  "Total will be: #{total}."
 end
